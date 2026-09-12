cedfb72b1175542ae9eeded46765e99683ff0b8d
TatianaNev24 <Tanefo24@gmail.com>
2026-09-12 06:33:09 -0500

    finalizado

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Calculator - Initial sum")
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+result = number1 + number2
+print(f"Result: {result}")
